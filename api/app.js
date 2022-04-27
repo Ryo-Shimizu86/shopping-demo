42339f03c5c4bd2f9c5d830ef210b0f553ee19f4
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-04-27 14:42:35 +0900

    deleted unused files

--- api/app.js
+++ api/app.js
@@ -5,9 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
 var staffsRouter = require("./routes/staffs");
 
 var livereload = require("livereload");
@@ -46,9 +43,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
 app.use("/staffs", staffsRouter);
 
 // catch 404 and forward to error handler
