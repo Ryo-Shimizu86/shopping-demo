95e71f4771b9671b9cf37f7327457b0a7a95e6b9
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-04-29 08:24:43 +0900

    changed staff insert

--- api/src/controllers/StaffController.js
+++ api/src/controllers/StaffController.js
@@ -11,43 +11,32 @@ exports.getStaffList = function (req, res) {
     .catch((error) => {
       console.log("error:", error);
       return res.json({
-        message: "Unable to fetch the record!",
+        message: "Unable to fetch the record!", // TODO check how to show message at react
       });
     });
-
-  // const sql = "SELECT * FROM Staffs";
-  // db.query(sql, function (err, data, fields) {
-  //   if (err) throw err;
-  //   res.send(data);
-  // });
 };
-// exports.getStaffList = function (req, res) {
-//   const sql = "SELECT * FROM Staffs";
-//   db.query(sql, function (err, data, fields) {
-//     if (err) throw err;
-//     res.send(data);
-//   });
-// };
 
 exports.addNewStaff = function (req, res) {
-  console.log(req.body);
-
   const { firstName, lastName, email, userName, password } = req.body.formData;
-  console.log(firstName, lastName, email, userName, password);
 
-  const staff = {
-    first_name: firstName,
-    last_name: lastName,
+  StaffModel.create({
+    firstName: firstName,
+    lastName: lastName,
     email: email,
     username: userName,
     password: password,
-    last_update: dayjs().format("YYYY-MM-DD h:mm:ss"), // TODO move format variable to common file
-  };
-
-  const sql = "INSERT INTO Staffs SET ?";
-  db.query(sql, staff, (err, data, fields) => {
-    if (err) throw err;
-    res.send(data);
-    console.log(data);
-  });
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  })
+    .then((result) => {
+      return res.json({
+        message: "Record created successfully!",
+      });
+    })
+    .catch((error) => {
+      console.log("error:", error);
+      return res.json({
+        message: "Unable to create a record",
+      });
+    });
 };
