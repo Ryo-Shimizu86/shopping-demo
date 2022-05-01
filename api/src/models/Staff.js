c5805b6d807bda543762ae96d8f729ea5341d2e7
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-05-01 09:14:39 +0900

    added model validation

--- api/src/models/Staff.js
+++ api/src/models/Staff.js
@@ -7,23 +7,56 @@ const Staff = sequelize.define("Staff", {
   firstName: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      isAlpha: true,
+      max: 50,
+      min: 2,
+    },
   },
   lastName: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      isAlpha: true,
+      max: 50,
+      min: 2,
+    },
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      // TODO unique check
+      notNull: true,
+      notEmpty: true,
+      max: 255,
+      isEmail: true,
+    },
   },
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      max: 50,
+      min: 2,
+    },
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      max: 255,
+      min: 8,
+    },
   },
   createdAt: {
     type: DataTypes.DATE,
